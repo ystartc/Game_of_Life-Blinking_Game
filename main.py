28ef7272f795a7265ab992d0ff48607b5f7fc771
Yulia S <startyupp@gmail.com>
2023-03-21 10:43:00 -0700

    added a new list that would keep a True False for new stage and return it to call in next generation as a new stage.

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from gameoflife import width, height, stage, print_stage, count_neighbors
 
 def init_stage(stage):
     for v_pos in range(0, height):
-      for h_pos in range(0, width):
-        if h_pos == 1:
-          stage[v_pos][h_pos] = True
-        else:
-          stage[v_pos][h_pos] = False
+        for h_pos in range(0, width):
+            if h_pos == 1:
+                stage[v_pos][h_pos] = True
+            else:
+                stage[v_pos][h_pos] = False
 
 # Generation Rules:
 # 1. Any live cell with < 2 neighbors dies
@@ -15,23 +15,44 @@ def init_stage(stage):
 # 4. Any dead cell with 3 neighbors comes alive
 
 def one_generation(stage):
+    new_stage = []
+    for i in range(height):
+        new_stage.append([False]*width)
     for v_pos in range(len(stage)):
-        for h_pos in range(len(v_pos)):
+        for h_pos in range(len(stage[v_pos])):
             neighbors = count_neighbors(stage, v_pos, h_pos)
             if not stage[v_pos][h_pos] and neighbors == 3:
-                stage[v_pos][h_pos] = True
+                new_stage[v_pos][h_pos] = True
             elif stage[v_pos][h_pos] and neighbors < 2:
-                stage[v_pos][h_pos] = False
+                new_stage[v_pos][h_pos] = False
             elif stage[v_pos][h_pos] and (neighbors == 2 or neighbors == 3):
-                stage[v_pos][h_pos] = True
+                new_stage[v_pos][h_pos] = True
             elif stage[v_pos][h_pos] and neighbors > 3:
-                stage[v_pos][h_pos] = False
+                new_stage[v_pos][h_pos] = False
+    return new_stage
                 
 
 init_stage(stage)
 print("First Generation:")
 print_stage(stage)
-one_generation(stage)
+
+stage = one_generation(stage)
 print("Second Generation:")
+print_stage(stage)
+
+stage = one_generation(stage)
+print("Third Generation:")
+print_stage(stage)
+
+stage = one_generation(stage)
+print("Fourth Generation:")
+print_stage(stage)
+
+stage = one_generation(stage)
+print("Fifth Generation:")
+print_stage(stage)
+
+stage = one_generation(stage)
+print("Six Generation:")
 print_stage(stage)
 
